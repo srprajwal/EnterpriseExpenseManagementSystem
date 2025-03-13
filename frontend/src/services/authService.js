4f26486700efb8d6fd40affed070b7f9d8411ef5
srpraju <srpraju1554@gmail.com>
2025-03-14 00:07:17 +0530

    AuthService file create

---
+++ frontend/src/services/authService.js
@@ -0,0 +1,23 @@
+// //   services/authService.js
+// import axios from 'axios';
+
+// const API_URL = 'your_backend_api_url'; // Replace with your actual API URL
+
+// const login = async (email, password) => {
+//   try {
+//     const response = await axios.post(`${API_URL}/auth/login`, {
+//       email,
+//       password,
+//     });
+//     return response.data; // Or handle as needed
+//   } catch (error) {
+//     throw error.response.data; // Or handle the error format from your API
+//   }
+// };
+
+// //   Other auth-related functions (register, forgot password, etc.) will go here
+
+// export default {
+//   login,
+//   //   ...other functions
+// };
